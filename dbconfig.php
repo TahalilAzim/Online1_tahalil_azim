d83698861e65cabf3f1f665e1fa7bf96b1e09000
Tahalil <tahalil@gmail.com>
2022-09-20 20:55:31 +0600

    updated

--- dbconfig.php
+++ dbconfig.php
@@ -3,5 +3,5 @@
   define('DB_USER', 'root');
   define('DB_PASSWORD', '');
   define('DB_HOST', 'localhost');
-  $link = mysqli_connect(DB_HOST, DB_USER , DB_PASSWORD, DB_NAME);
+  $con = mysqli_connect(DB_HOST, DB_USER , DB_PASSWORD, DB_NAME);
 ?>
