2234846f4d2b9fd370b26b573caddeb59923d742
Tahalil <tahalil@gmail.com>
2022-09-20 20:22:24 +0600

    config file

---
+++ dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+  define('DB_NAME', 'airplane_management');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', '');
+  define('DB_HOST', 'localhost');
+  $link = mysqli_connect(DB_HOST, DB_USER , DB_PASSWORD, DB_NAME);
+?>
