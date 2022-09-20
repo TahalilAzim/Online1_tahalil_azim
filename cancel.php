6199c96057a22d38216d5e0520b622e27362becf
TahalilAzim <105869446+TahalilAzim@users.noreply.github.com>
2022-09-20 21:11:27 +0600

    Delete cancel.php

--- cancel.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once 'dbconfig.php';
-  if(isset($_POST['send'])){
-    $id = $_POST['id'];
-    $sql = "DELETE FROM `ticket_booking` WHERE ID = $id";
-    $con->query($sql);
-    header("Location: index.php");
-  }
-?>
